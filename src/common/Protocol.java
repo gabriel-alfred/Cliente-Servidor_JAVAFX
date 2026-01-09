63822edfbd3504599ade9ed092a6a25992a1c22f
Alfred Gabriiel <alfredgabrielciucaandrei@gmail.com>
2026-01-09 09:13:36 +0100

    feat(common): Añadir protocolo y modelos de datos (User, Ticket, Message)

--- src/common/Protocol.java
+++ src/common/Protocol.java
@@ -1,4 +1,22 @@
 package common;
 
 public class Protocol {
+    // Configuration
+    public static final int PORT = 9000;
+
+    // Command Types (Requests)
+    public static final int CMD_LOGIN = 1;
+    public static final int CMD_LIST_TICKETS = 2;
+    public static final int CMD_CREATE_TICKET = 3;
+    public static final int CMD_UPDATE_TICKET = 4;
+    public static final int CMD_LOGOUT = 5;
+
+    // Response Status
+    public static final int STATUS_OK = 200;
+    public static final int STATUS_ERROR = 400;
+    public static final int STATUS_UNAUTHORIZED = 401;
+
+    // Error Messages
+    public static final String ERR_LOGIN_FAILED = "Credenciales incorrectas";
+    public static final String ERR_USER_EXISTS = "El usuario ya existe";
 }
--- src/common/model/Message.java
+++ src/common/model/Message.java
@@ -1,4 +1,21 @@
 package common.model;
 
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int command;      // Protocol.CMD_... or Protocol.STATUS_...
+    private Object object;    // Payload (User, Ticket, List<Ticket>, String error, etc.)
+
+    public Message(int command, Object object) {
+        this.command = command;
+        this.object = object;
+    }
+
+    public int getCommand() { return command; }
+    public void setCommand(int command) { this.command = command; }
+
+    public Object getObject() { return object; }
+    public void setObject(Object object) { this.object = object; }
 }
